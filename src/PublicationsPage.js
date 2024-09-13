bd39e467d130b5189415ac2feceb863a76e2f481
Kyle Swanson <swansonk.14@gmail.com>
2024-09-13 11:54:08 -0700

    Adding European Heart Journal review

--- src/PublicationsPage.js
+++ src/PublicationsPage.js
@@ -10,6 +10,17 @@ class PublicationsPage extends Component {
                 <h1>Publications</h1>
 
                 <h2 className="publicationTitle"><a
+                    href="https://academic.oup.com/eurheartj/advance-article/doi/10.1093/eurheartj/ehae519/7756511?utm_source=authortollfreelink&utm_campaign=eurheartj&utm_medium=email&guestAccessKey=d8ce1cae-1734-4675-915c-81835fd06563"> Clinical
+                    trials in-a-dish for cardiovascular medicine</a></h2>
+                <p className="authorsText">Xuekun Wu, <span
+                    className="selfAuthor">Kyle Swanson</span>, Zehra Yildirim, Wenqiang Liu, Ronglih Liao, and Joseph C
+                    Wu.
+                </p>
+                <p className="journalText"><span className="journalName">European Heart Journal</span>, 2024.</p>
+
+                <Divider/>
+
+                <h2 className="publicationTitle"><a
                     href="https://www.nature.com/articles/s41592-024-02354-y"> Language models for biological research:
                     a primer</a></h2>
                 <p className="authorsText">Elana Simon<sup>*</sup>, <span
