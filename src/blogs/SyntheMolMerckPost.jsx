a78cbf26c8862c8129304c203fe8f26d6f856a59
Kyle Swanson <swansonk.14@gmail.com>
2026-03-22 15:09:14 -0700

    Adding authors

--- src/blogs/SyntheMolMerckPost.jsx
+++ src/blogs/SyntheMolMerckPost.jsx
@@ -9,9 +9,17 @@ class SyntheMolMerckPost extends Component {
       <div className="Page blog-post">
         <p><Link to="/blog">&larr; Back to Blog</Link></p>
         <h1 className="sectionTitle">Generative AI to Design Small Molecule Therapeutics: Lessons from a Stanford/Merck collaboration</h1>
-        <p className="blog-date">March 22, 2026</p>
+        <p className="blog-date">March 30, 2026</p>
 
-        <p>I was fortunate to collaborate with Merck to apply SyntheMol-RL, a generative AI model I built at Stanford to design small molecule drugs, to two of their internal drug discovery programs. This post is a summary of the project and the lessons we learned.</p>
+        <p style={{ textAlign: "center" }}>
+          Kyle Swanson<sup>1</sup>, James Zou<sup>1,2</sup>, Maria Chiriac<sup>3</sup>, Alan Cheng<sup>3</sup>
+        </p>
+        <p>
+          <sup>1</sup>Department of Computer Science, Stanford University<br />
+          <sup>2</sup>Department of Biomedical Data Science, Stanford University<br />
+          <sup>3</sup>Discovery Chemistry, Merck &amp; Co., Inc., South San Francisco, CA, USA
+        </p>
+
       </div>
     );
   }
